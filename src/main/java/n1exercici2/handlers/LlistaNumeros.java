b69c03998b5ba6489c7d965456de7becc0c220d8
Claudio Cordero <cordero.guiza@gmail.com>
2024-03-11 12:24:45 +0100

    First commit

---
+++ src/main/java/n1exercici2/handlers/LlistaNumeros.java
@@ -0,0 +1,58 @@
+package n1exercici2.handlers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LlistaNumeros {
+
+	private List<Integer> numeros1 = new ArrayList<Integer>();
+	private List<Integer> numeros2 = new ArrayList<Integer>();
+	
+	private List<Integer> emplenarLlista(){
+		
+		numeros1.add(10);
+		numeros1.add(20);
+		numeros1.add(30);
+		numeros1.add(40);
+		
+		return numeros1;
+	}
+	
+	private List<Integer> invertirLlista(){
+		
+		Collections.reverse(numeros1);
+		
+		return numeros1;
+	}
+	
+	private List<Integer> inserirLlista(){
+		
+		numeros2.addAll(numeros1);
+		
+		return numeros2;
+	}
+	
+	private List<Integer> recorrerInserirLlista(){
+		
+		
+		ListIterator<Integer> listIt = numeros1.listIterator();
+		
+		while(listIt.hasNext()) {
+			
+			numeros2.add(listIt.next())	;
+		}
+		
+		return numeros2;
+	}
+	
+	public void mostrarExercici() {
+		
+		System.out.println(emplenarLlista().toString());
+		System.out.println(invertirLlista().toString());
+		System.out.println(inserirLlista().toString());
+		System.out.println(recorrerInserirLlista().toString());
+		
+	}
+}
---
+++ src/main/java/n1exercici2/main/LlistaNumerosMain.java
@@ -0,0 +1,14 @@
+package n1exercici2.main;
+
+import n1exercici2.handlers.LlistaNumeros;
+
+public class LlistaNumerosMain {
+
+	public static void main(String[] args) {
+		
+		LlistaNumeros demo = new LlistaNumeros();
+		demo.mostrarExercici();
+	}		
+}
+
+
